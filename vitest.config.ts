54c7057146508a35bab386d44ca911e2f95fb7a5
Piotr Buchman <kontakt@pbuchman.com>
2025-12-28 11:56:19 +0100

    Decrease branch coverage threshold from 90 to 80

--- vitest.config.ts
+++ vitest.config.ts
@@ -81,7 +81,7 @@ export default defineConfig({
       ],
       thresholds: {
         lines: 90,
-        branches: 90,
+        branches: 80,
         functions: 90,
         statements: 90,
       },
